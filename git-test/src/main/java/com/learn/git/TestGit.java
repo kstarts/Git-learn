6ff5cba257c64214899aff1115cebdf832fe360f
kc <943766105@qq.com>
2022-10-18 20:21:25 +0800

    useGit 2

--- git-test/src/main/java/com/learn/git/TestGit.java
+++ git-test/src/main/java/com/learn/git/TestGit.java
@@ -7,5 +7,6 @@ package com.learn.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git2");
     }
 }
