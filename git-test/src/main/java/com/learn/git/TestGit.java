428a6594b1d63b741453fd6e6c63e5ff7cded6d7
kc <943766105@qq.com>
2022-10-18 20:17:23 +0800

    useGit

---
+++ git-test/src/main/java/com/learn/git/TestGit.java
@@ -0,0 +1,11 @@
+package com.learn.git;
+
+/**
+ * @author kecheng
+ * @create 2022-10-18 20:10
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
